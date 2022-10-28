5adc4bbe9c6ad74627e8903371e06aa733ce216a
Shentong Yu <sy2794@nyu.edu>
2022-10-28 16:32:56 -0400

    Midterm_Code

---
+++ CreativeCoding_Midterm_Shentong/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(600,600);
+  background(0);
+  // put setup code here
+}
+
+function draw() {
+  // put drawing code here
+  fill(127);
+  rect(100,100,100,100)
+}
