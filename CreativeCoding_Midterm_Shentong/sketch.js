2937e8ef80ec4d2ddf6219ff68bdc79a918fba45
Shentong Yu <sy2794@nyu.edu>
2022-10-29 14:17:39 -0400

    background

--- CreativeCoding_Midterm_Shentong/sketch.js
+++ CreativeCoding_Midterm_Shentong/sketch.js
@@ -1,11 +1,27 @@
+/*
+red = (203, 51, 31);
+blue = (13, 50, 132);
+yellow = (253, 240, 80);
+*/
+
 function setup() {
-  createCanvas(800,800);
-  background(0);
-  // put setup code here
+  createCanvas(1500, 900);
+  background(255);
 }
 
 function draw() {
-  // put drawing code here
-  fill(127);
-  rect(100,100,100,100)
+  //background
+  for (let i = 0; i < 1500; i += 10){
+    for (let j =0 ; j < 900; j += 10){
+      if ((j%20)==0){
+      fill(203, 51, 31);
+      noStroke();
+      ellipse(i,j,7,7);
+      } else {
+      fill(203, 51, 31);
+      noStroke();
+      ellipse(i+5,j,7,7);
+      }
+    }
+  }
 }
