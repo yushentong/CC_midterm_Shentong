86cd7fb43e3a536c7584247f4e25717cdcbb2acc
Shentong Yu <sy2794@nyu.edu>
2022-10-31 09:28:12 -0400

    Andy Warhol Background

--- CreativeCoding_Midterm_Shentong/eyes_class.js
+++ CreativeCoding_Midterm_Shentong/eyes_class.js
@@ -5,7 +5,7 @@ class Eyes{
 		this.y = y;
 	}
 
-	background(color01, color02){
+	/*background(color01, color02){
 		for (let i = 0; i < 300; i += 10){
 			if ((i%20)==0){
 				fill(color01);
@@ -15,43 +15,54 @@ class Eyes{
 				rect(this.x, this.y, 300-i, 300-i);
 			}
 		}
-	}
+	}*/
 
-	display(){
+	display(color01, color02, color03, color04){
+
 		noStroke();
 
-		fill(225, 232, 237);//color of eye base
+		fill(color01);//background of the frame
+		rect(this.x, this.y, 300, 300)
+
+		fill(color04);//eyewhite pop art mimic
+		ellipse(this.x-60, this.y, 120, 200);//left
+		ellipse(this.x+70, this.y, 120, 200);//right
+
+		fill(color03);//color of eye base
 		ellipse(this.x-65, this.y, 120, 200);//left
 		ellipse(this.x+65, this.y, 120, 200);//right
 
-		fill(245, 248, 250);//eye white
+		fill(color04);//eye white on the eye base
 		ellipse(this.x-65, this.y, 105, 180);//left
 		ellipse(this.x+65, this.y, 105, 180);//right
 
-		let locLeftX = map (mouseX, 0, width, this.x - 65 - 30, this.x - 65 + 30 );
-		let locRightX = map (mouseX, 0, width, this.x + 65 - 30, this.x + 65 + 30 );
+		let locLeftX = map (mouseX, 0, width, this.x - 65 - 30, this.x - 65 + 20 );
+		let locRightX = map (mouseX, 0, width, this.x + 65 - 30, this.x + 65 + 20 );
 		let locY = map (mouseY, 0, width, this.y - 30, this.y + 30 );
 
-		//Left Eyeball
-		fill(136, 153, 166);
+		//Eyeball
+		fill(color01);//eyeball base pop art mimic
+		ellipse(locLeftX+5, locY, 60, 60);
+		ellipse(locRightX+5, locY, 60, 60);
+
+		fill(color02);//eyeball base
 		ellipse(locLeftX, locY, 60, 60);
-
-		fill(41, 47, 51);
-		ellipse(locLeftX, locY, 45, 45);
-
-		fill(245, 248, 250);
-		ellipse(locLeftX+10, locY-15, 10, 10);
-
-		//Right Eyeball
-		fill(136, 153, 166);
 		ellipse(locRightX, locY, 60, 60);
 
-		fill(41, 47, 51);
-		ellipse(locRightX, locY, 45, 45);
+		fill(color01);//eyeball iris
+		ellipse(locLeftX, locY, 35, 35);
+		ellipse(locRightX, locY, 35, 35);
 
-		fill(245, 248, 250);
-		ellipse(locRightX+10, locY-15, 10, 10);
+		fill(color03);//eyeball highlight
+		ellipse(locLeftX+10, locY-10, 20, 20);
+		ellipse(locRightX+10, locY-10, 20, 20);
 
+		//Decoration
+		noFill();
+		strokeWeight(10);
+		stroke(color03);
+		arc(this.x+65, this.y, 150, 240, 0, 0.5*PI);
+		arc(this.x-65, this.y, 150, 240, PI, 1.5*PI);
 
 
 	}
--- CreativeCoding_Midterm_Shentong/sketch.js
+++ CreativeCoding_Midterm_Shentong/sketch.js
@@ -32,8 +32,38 @@ function setup() {
   popcorn22 = new Popcorn();
   popcorn23 = new Popcorn();
   popcorn24 = new Popcorn();
+  popcorn25 = new Popcorn();
+  popcorn26 = new Popcorn();
+  popcorn27 = new Popcorn();
+  popcorn28 = new Popcorn();
+  popcorn29 = new Popcorn();
+  popcorn30 = new Popcorn();
+  popcorn30 = new Popcorn();
+  popcorn31 = new Popcorn();
+  popcorn32 = new Popcorn();
+  popcorn33 = new Popcorn();
+  popcorn34 = new Popcorn();
+  popcorn35 = new Popcorn();
+  popcorn36 = new Popcorn();
+  popcorn37 = new Popcorn();
+  popcorn38 = new Popcorn();
+  popcorn39 = new Popcorn();
+  popcorn40 = new Popcorn();
+  popcorn41 = new Popcorn();
+  popcorn42 = new Popcorn();
+  popcorn43 = new Popcorn();
+  popcorn44 = new Popcorn();
 
-  eye01 = new Eyes(450, 450);
+  //from top to down, left to right
+  eyes01 = new Eyes(150, 150);
+  eyes02 = new Eyes(450, 150);
+  eyes03 = new Eyes(750, 150);
+  eyes04 = new Eyes(150, 450);
+  eyes05 = new Eyes(450, 450);
+  eyes06 = new Eyes(750, 450);
+  eyes07 = new Eyes(150, 750);
+  eyes08 = new Eyes(450, 750);
+  eyes09 = new Eyes(750, 750);
 }
 
 function draw() {
@@ -53,8 +83,17 @@ function draw() {
     }
   }// end of background
 
-  eye01.background((color(30,255,20)),(color(0,0,255)));
-  eye01.display();
+  eyes01.display((color(235, 1, 0)),(color(250, 232, 208)), (color(5, 125, 192)), (color(230, 179, 0)));
+  eyes02.display((color(4, 207, 217)),(color(252, 220, 0)), (color(184, 3, 1)), (color(251, 219, 220)));
+  eyes03.display((color(7, 180, 0)),(color(251, 201, 6)), (color(230, 3, 133)), (color(1, 139, 1)));
+  eyes04.display((color(9, 133, 85)),(color(260, 203, 167)), (color(226, 1, 116)), (color(11, 179, 207)));
+  eyes05.display((color(236, 1, 119)),(color(178, 3, 2)), (color(1, 1, 60)), (color(202, 179, 3)));
+  eyes06.display((color(255, 238, 212)),(color(255, 211, 1)), (color(204, 1, 0)), (color(102, 212, 179)));
+  eyes07.display((color(1, 167, 209)),(color(240, 149, 0)), (color(0, 200, 231)), (color(254, 225, 227)));
+  eyes08.display((color(0, 134, 190)),(color(127, 226, 212)), (color(1, 153, 16)), (color(235, 1, 135)));
+  eyes09.display((color(214, 1, 0)),(color(252, 226, 3)), (color(0)), (color(253, 221, 165)));
+
+  //eye05.display((color(195, 170, 0)),(color(184, 2, 0)));
 
   //let gravity = createVector(0, 1);
 
@@ -153,4 +192,84 @@ function draw() {
   popcorn24.display(4);
   popcorn24.update();
   popcorn24.checkEdges();
+
+  popcorn25.display(1);
+  popcorn25.update();
+  popcorn25.checkEdges();
+
+  popcorn26.display(2);
+  popcorn26.update();
+  popcorn26.checkEdges();
+
+  popcorn27.display(3);
+  popcorn27.update();
+  popcorn27.checkEdges();
+
+  popcorn28.display(4);
+  popcorn28.update();
+  popcorn28.checkEdges();
+
+  popcorn29.display(1);
+  popcorn29.update();
+  popcorn29.checkEdges();
+
+  popcorn30.display(2);
+  popcorn30.update();
+  popcorn30.checkEdges();
+
+  popcorn31.display(3);
+  popcorn31.update();
+  popcorn31.checkEdges();
+
+  popcorn32.display(4);
+  popcorn32.update();
+  popcorn32.checkEdges();
+
+  popcorn33.display(1);
+  popcorn33.update();
+  popcorn33.checkEdges();
+
+  popcorn34.display(2);
+  popcorn34.update();
+  popcorn34.checkEdges();
+
+  popcorn35.display(3);
+  popcorn35.update();
+  popcorn35.checkEdges();
+
+  popcorn36.display(4);
+  popcorn36.update();
+  popcorn36.checkEdges();
+
+  popcorn37.display(1);
+  popcorn37.update();
+  popcorn37.checkEdges();
+
+  popcorn38.display(2);
+  popcorn38.update();
+  popcorn38.checkEdges();
+
+  popcorn39.display(3);
+  popcorn39.update();
+  popcorn39.checkEdges();
+
+  popcorn40.display(4);
+  popcorn40.update();
+  popcorn40.checkEdges();
+
+  popcorn41.display(1);
+  popcorn41.update();
+  popcorn41.checkEdges();
+
+  popcorn42.display(2);
+  popcorn42.update();
+  popcorn42.checkEdges();
+
+  popcorn43.display(3);
+  popcorn43.update();
+  popcorn43.checkEdges();
+
+  popcorn44.display(4);
+  popcorn44.update();
+  popcorn44.checkEdges();
 }
